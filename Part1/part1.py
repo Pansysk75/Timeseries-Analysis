c3168ce9c93da6879eff93baf577fe97dfd0a0db
Pansysk75 <pansysk75@gmail.com>
2022-12-28 13:51:06 +0200

    Add batch ARIMA test

--- Part1/part1.py
+++ Part1/part1.py
@@ -3,8 +3,13 @@ import numpy as np
 
 from utilities import *
 
+import pmdarima as pmd
+
 from pathlib import Path
 
+import matplotlib
+# matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 # Import datasets
 dataset_path = "../Datasets/"
@@ -18,13 +23,66 @@ for dataset in dataset_files:
 # Trim datasets to size of smallest dataset
 data = trim_datasets(data, alignment="random")
 
+
+
 Path("plots").mkdir(exist_ok=True)
 
-# Plot stats for non-stationary timeseries
-for dataset_name, dataset in data.items():
-    plot_timeseries_stats(dataset, dataset_name, "plots")
+# # Plot stats for non-stationary timeseries
+# for dataset_name, dataset in data.items():
+#     plot_timeseries_stats(xV = dataset,
+#                           name = dataset_name,
+#                           savepath = "plots")
 
 # Take first difference and plot again
+data_fd = {}
 for dataset_name, dataset in data.items():
-    dataset = np.diff(dataset, 3)
-    plot_timeseries_stats(dataset, dataset_name+"(1st_diff)", "plots")
+    data_fd[dataset_name] = np.diff(dataset, 1)
+    # plot_timeseries_stats(xV = data_fd[dataset_name],
+    #                       name = dataset_name+"(1st_diff)",
+    #                       savepath = "plots")
+
+# # Take log + first difference and plot again
+# data_fd_log = {}
+# for dataset_name, dataset in data.items():
+#     data_fd_log[dataset_name] = np.diff(np.log(dataset), 1)
+#     plot_timeseries_stats(data_fd_log[dataset_name], dataset_name+"(1st_diff_log)", "plots")
+
+# Split into train and test datasets
+train_set = {}
+test_set = {}
+
+for name, xV in data.items():
+    train_set[name], test_set[name] = split_dataset(xV)
+
+# Fit AR models
+xV = train_set["eruption1989.dat"]
+best_model, _ = batch_arima_test(xV, p_min=0, p_max=3,
+                     q_min=0, q_max=3,
+                     show=True)
+
+
+best_model.plot_diagnostics()
+print("Summary for trained model:")
+print(best_model.summary())
+xV_pred = best_model.predict()
+
+xV_test = test_set["eruption1989.dat"]
+res2 = best_model.apply(xV_test)
+print("Summary for test data:")
+print(res2.summary())
+xV_test_pred = res2.predict()
+res2.plot_diagnostics()
+
+
+plt.figure()
+plt.plot(xV)
+plt.plot(xV_pred)
+plt.title(f'NRMSE: {nrmse(xV, xV_pred)}')
+plt.show()
+
+plt.figure()
+plt.plot(xV_test)
+plt.plot(xV_test_pred)
+plt.title(f'NRMSE: {nrmse(xV, xV_pred)}')
+plt.show()
+
--- Part1/utilities.py
+++ Part1/utilities.py
@@ -2,9 +2,13 @@ from typing import Literal
 import random
 
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-
+import pmdarima as pmd
 ###!pip install numpy matplotlib statsmodels
 
 
@@ -35,6 +39,17 @@ def trim_datasets(data, alignment: Literal['start', 'end', 'random']):
     return trimmed_data
 
 
+def split_dataset(xV, split_ratio = 0.8):
+    split_idx = round(len(xV) * split_ratio)
+    return (xV[0:split_idx], xV[split_idx+1:])
+
+def rmse(xV1, xV2):
+    return np.sqrt(np.square(np.subtract(xV1, xV2)).mean())
+
+def nrmse(xV1, xV2):
+    '''Normalized rmse (normalized by std of xV1)'''
+    return rmse(xV1,xV2)/np.std(xV1)
+
 def plot_timeseries_stats(xV, name, savepath=None):
     '''Plot timeseries, auto-correlation and partial auto-correlation.'''
     fig = plt.figure(figsize=(12,8))
@@ -43,10 +58,45 @@ def plot_timeseries_stats(xV, name, savepath=None):
     plt.subplot(2, 1, 1).plot(xV)
     # plt.savefig("plots/timeseries/" + dataset_name + ".png")
 
+    # From statsmodels module.
     plot_acf(xV, zero=False, lags=10, ax=plt.subplot(2,2,3))
+
+    # From statsmodel module (yule-walker method).
     plot_pacf(xV, zero=False, lags=10, ax=plt.subplot(2,2,4), method='ywm')   
 
     if(savepath != None):
         plt.savefig(savepath + "/" + name + ".png")
 
     plt.show()
+
+def batch_arima_test(xV, p_min=0, p_max=5, q_min=0, q_max=5, d=0, show=False):
+    '''Fits many arima models and returns the one with
+    the smalles aic. Returns a statsmodels.tsa.ARIMA model and 
+    a list of dictionaries containing "p", "q" and "aic" '''
+    aic_values = []
+    best_model = None
+    best_aic = float('inf')
+    for p in range(p_min, p_max+1):
+        for q in range(q_min, q_max+1):
+            sm.tsa.arima
+            model = sm.tsa.ARIMA(xV, order=(p, d, q)).fit()
+            aic = model.aicc
+            aic_values.append({"p":p, "q":q, "aic":aic})
+            print(f"Fitted ({p}, {d}, {q}), aic={aic}")
+            if aic < best_aic:
+                best_aic = aic
+                best_model = model
+    if show:
+        plt.figure()
+        df = pd.DataFrame(aic_values).pivot(index="p", columns="q", values="aic")
+        ax = sns.heatmap(df, fmt=".1f",
+          norm=matplotlib.colors.PowerNorm(0.3),
+        cmap="gray_r",annot=True)
+        ax.invert_yaxis()
+        plt.show()
+    
+    return best_model, aic_values
+
+
+# def generate_model_report(arima_model, train_data):
+#     arima_model.predict
